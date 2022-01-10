f7dc9231d619c20eb5b7f5a17b038c5497809a7c
Canoa furada =) <88066241+canoafurada2021@users.noreply.github.com>
2022-01-10 18:40:00 -0300

    Create contador.dart

---
+++ contador.dart
@@ -0,0 +1,12 @@
+void main() {
+  int contador = 0;
+
+  // != significa "diferente"
+  // ++ significa +1
+
+  do {
+    print("O valor do contador é $contador");
+    contador++;
+  } while (contador <= 65); //escolher determinado valor para que o contador execute a função
+  print("Contador finalizado com sucesso!");
+}
